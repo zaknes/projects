daadcadc4b316541b8328bff780d15d767e6a649
Zak Nesler <zaknesler@gmail.com>
2018-11-29 22:18:56 -0500

    Update google project

--- css/tailwind/recreations/google/src/js/app.js
+++ css/tailwind/recreations/google/src/js/app.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 
 Vue.component('search-results', require('./components/SearchResults.vue'))
 Vue.component('tab-item', require('./components/TabItem.vue'))
-
 Vue.component('tabs', require('./components/Tabs.vue'))
 
 const app = new Vue({
